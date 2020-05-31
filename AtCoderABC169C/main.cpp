c9a63cf3a27c5e8553e616b496361b07b5323b22
ohsuyuki <papu8osu@gmail.com>
2020-05-31 22:11:09 +0900

    Initial Commit

---
+++ AtCoderABC169C/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AtCoderABC169C
+//
+//  Created by yuuki oosu on 2020/05/31.
+//  Copyright © 2020 yuuki oosu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
